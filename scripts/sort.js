f5d82dfbc5d4267291ffb9506f263b9c0a1bbd78
Mick van Gelderen <mickvangelderen@gmail.com>
2016-06-03 00:11:10 +0200

    Sort line files

--- scripts/sort.js
+++ scripts/sort.js
@@ -4,13 +4,37 @@ import glob from 'glob'
 import sortObject from 'sort-object-circular'
 import { join } from 'path'
 
-// function sortLines(string) {
-// 	return string
-// 	.split(EOL)
-// 	.sort()
-// 	.join(EOL)
-// 	.trim(EOL) + EOL
-// }
+// Sort groups of non-empty lines.
+function sortLines(input) {
+	const groups = [
+		[]
+	]
+	let groupIndex = 0
+	let groupEmpty = true
+	const regex = /([^\r\n]*)(\r?\n|\r)/g
+	let match = null
+	while(match = regex.exec(input)) { // eslint-disable-line no-cond-assign
+		const [ , line, ending ] = match
+		if ((line === '') !== groupEmpty) {
+			groups[++groupIndex] = []
+			groupEmpty = !groupEmpty
+		}
+		groups[groupIndex].push({ line, ending })
+	}
+	return groups
+	.map((parts, index) =>
+		(
+			index % 2 === 0
+				? parts
+					.map(({ ending }) => ending)
+				: parts
+					.sort((l, r) => l.line > r.line)
+					.map(({ line, ending }) => line + ending)
+		)
+		.join('')
+	)
+	.join('')
+}
 
 const workingDirectoryPath = join(__dirname, '..')
 
@@ -30,4 +54,20 @@ const jsonResults = glob.sync('**/{.babelrc,*.json}', {
 	})
 })
 
-export default jsonResults
+const linesResults = glob.sync('**/{.gitignore,.npmignore}', {
+	cwd: workingDirectoryPath,
+	ignore: [
+		'**/node_modules/**',
+		'build/**'
+	]
+}).map(filePath => {
+	return createFileTransformerSync(
+		buffer => sortLines(buffer.toString())
+	)({
+		workingDirectoryPath,
+		inputFilePath: filePath,
+		outputFilePath: filePath
+	})
+})
+
+export default [ ...jsonResults, ...linesResults ]
