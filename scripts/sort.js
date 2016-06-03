6207915ad91599cb6c6fc00cd934f9bf24fb9bff
Mick van Gelderen <mickvangelderen@gmail.com>
2016-06-03 10:59:38 +0200

    Make sort lines work on files not terminated by a newline

--- scripts/sort.js
+++ scripts/sort.js
@@ -2,35 +2,50 @@ import createFileTransformerSync from './lib/createFileTransformerSync'
 import createJsonTransformer from './lib/createJsonTransformer'
 import glob from 'glob'
 import sortObject from 'sort-object-circular'
+import { EOL } from 'os'
 import { join } from 'path'
 
 // Sort groups of non-empty lines.
 function sortLines(input) {
-	const groups = [
-		[]
-	]
+	// Split the input string into parts.
+	const parts = input.split(/(\r?\n|\r)/)
+
+	// If the last part is empty, the last line contained a newline.
+	if (parts[parts.length - 1] === '') {
+		// Delete the empty match.
+		parts.pop()
+	} else {
+		// Append a newline.
+		parts[parts.length] = (parts.length - 2) >= 0
+			? parts[parts.length - 2] // Use the previous newline.
+			: EOL // Default to the standard newline for the current os. 
+	}
+
+	// Split the parts into groups of lines separated by a single newline.
+	const groups = [ [] ]
 	let groupIndex = 0
-	let groupEmpty = true
-	const regex = /([^\r\n]*)(\r?\n|\r)/g
-	let match = null
-	while(match = regex.exec(input)) { // eslint-disable-line no-cond-assign
-		const [ , line, ending ] = match
-		if ((line === '') !== groupEmpty) {
+	let groupTypeIsEmpty = true
+	let i = 0
+	while(i < parts.length) {
+		const line = parts[i++]
+		const ending = parts[i++]
+		if ((line === '') !== groupTypeIsEmpty) {
 			groups[++groupIndex] = []
-			groupEmpty = !groupEmpty
+			groupTypeIsEmpty = !groupTypeIsEmpty
 		}
 		groups[groupIndex].push({ line, ending })
+
 	}
+
+	// Concatentate groups, sort odd (non-empty line) groups.
 	return groups
 	.map((parts, index) =>
 		(
 			index % 2 === 0
 				? parts
-					.map(({ ending }) => ending)
-				: parts
-					.sort((l, r) => l.line > r.line)
-					.map(({ line, ending }) => line + ending)
+				: parts.sort((l, r) => l.line > r.line)
 		)
+		.map(({ line, ending }) => line + ending)
 		.join('')
 	)
 	.join('')
